609ec2b4b49983efa5f5deb838c500935a0a0ddf
Ezeko <ezeko2017@gmail.com>
2020-05-20 17:08:31 +0100

    feat
    add written screens to navigation

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -2,6 +2,9 @@ import {createStackNavigator} from "react-navigation-stack";
 import {createAppContainer } from "react-navigation";
 
 import Slider from "../screens/Slider/index";
+import SignUp from "../screens/SignUp";
+import OTP from "../screens/OTP";
+import SignIn from "../screens/SignIn"
 
 const AppNavigator = createStackNavigator(
     {
@@ -11,6 +14,24 @@ const AppNavigator = createStackNavigator(
               headerShown: false
             }
           },
+        SignUp: {
+          screen: SignUp,
+          navigationOptions: {
+            headerShown: false
+          }
+        },
+        OTP: {
+          screen: OTP,
+          navigationOptions: {
+            headerShown: false
+          }
+        },
+        SignIn: {
+          screen: SignIn,
+          navigationOptions: {
+            headerShown: false
+          }
+        },
     },
     {
         initialRouteName: "Slider"
---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {
+    TextInput,
+    Text,
+    View,
+    Button
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+export default class SignIn extends Component {
+    
+}
