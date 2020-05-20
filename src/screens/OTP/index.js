98fd95a78972f76cf7e71097a3a6cd9e3ee08869
Ezeko <ezeko2017@gmail.com>
2020-05-20 16:14:21 +0100

    feat
    create one time code screen

---
+++ src/screens/OTP/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+    TextInput,
+    Text,
+    View,
+    Button
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+
+export default class OTP extends Component {
+  constructor (props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <View>
+        <Text>Welcome David, </Text>
+        <Text>Kindly enter your 8 digits pin from your email...</Text>
+        <KeyboardAwareScrollView>
+          <TextInput
+            returnKeyType = 'send'
+            keyboardType = 'number-pad'
+            textContentType = 'oneTimeCode'
+            maxLength = {8}
+          />
+        </KeyboardAwareScrollView>
+
+      </View>
+    );
+  }
+}
