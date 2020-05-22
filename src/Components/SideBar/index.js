9834887e57d7f75875ae80e813331666c67b6e35
Ezeko <ezeko2017@gmail.com>
2020-05-22 14:43:30 +0100

    'bug'
    complete sidebar contents to have the same elements as UI

--- src/Components/SideBar/index.js
+++ src/Components/SideBar/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Text, View, TouchableOpacity, Image} from 'react-native';
 import {Icon} from 'react-native-elements';
-
+import SideBar from '../SideContent/index';
 
 export default class Sidebar extends Component {
     constructor (props) {
@@ -19,6 +19,98 @@ export default class Sidebar extends Component {
                     type = 'ionicon'
                     name='chevron-back-outline'/>
                 </View>
+
+                {
+                //vertical line in between
+                }
+                <View></View>
+
+                {
+                    //side menu detailed contents
+                }
+                <View>
+                    <SideBar 
+                    text = 'Home'
+                    IconName = 'menu'
+                    />
+
+                    <SideBar 
+                    text = 'Transfer Funds'
+                    IconName = 'wallet'
+                    />
+
+                    <SideBar 
+                    text = 'Card Requests'
+                    IconName = 'menu'
+                    />
+
+                    <SideBar 
+                    text = 'Cards'
+                    IconName = 'wallet'
+                    />
+                    
+                    <SideBar 
+                    text = 'Lifestyle & Travel'
+                    IconName = 'menu'
+                    />
+
+                    <SideBar 
+                    text = 'Self Service'
+                    IconName = 'wallet'
+                    />
+
+                    <SideBar 
+                    text = 'Loans'
+                    IconName = 'menu'
+                    />
+
+                    <SideBar 
+                    text = 'Notifications'
+                    IconName = 'wallet'
+                    />
+
+                    <SideBar 
+                    text = 'My profile'
+                    IconName = 'menu'
+                    />
+                    
+                    <SideBar 
+                    text = 'Log out'
+                    IconName = 'wallet'
+                    route = ''
+                    />
+
+                    <SideBar 
+                    text = 'Bill Payment'
+                    IconName = 'wallet'
+                    />
+
+                    <SideBar 
+                    text = 'Airtime'
+                    IconName = 'wallet'
+                    />
+
+
+                    <SideBar 
+                    text = 'Savings'
+                    IconName = 'wallet'
+                    route = ''
+                    />
+
+                    <SideBar 
+                    text = 'Investment'
+                    IconName = 'wallet'
+                    route = ''
+                    />
+            
+                    <SideBar 
+                    text = 'Cryptocurrency'
+                    IconName = 'wallet'
+                    route = ''
+                    />
+
+                </View>
+
             </View>
         )
     }
--- src/Components/SideContent/index.js
+++ src/Components/SideContent/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import {Icon} from 'react-native-elements';
 import style from './style'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 export default class SideBarContent extends Component {
@@ -12,14 +13,17 @@ export default class SideBarContent extends Component {
     
     
     render() {
+        const {route} = this.props.route
         return(
             <View>
-                <Icon
-                type = 'ionicon'
-                name = {this.props.IconName} 
-                />
+                <TouchableOpacity onPress = {() => this.props.navigation.navigate(route)}>
+                    <Icon
+                    type = 'simple-line-icon'
+                    name = {this.props.IconName} //will take value from the parent component
+                    />
 
-                <Text>{this.props.text}</Text>
+                    <Text>{this.props.text}</Text> 
+                </TouchableOpacity>
             </View>
         )
     }
