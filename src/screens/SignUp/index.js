6a77c084c9347dd584ee70b3e229936df2c5f0fb
Ezeko <ezeko2017@gmail.com>
2020-05-29 13:45:44 +0100

    chunk

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -3,11 +3,13 @@ import {
   TextInput,
   View,
   Text,
-  StatusBar
+  StatusBar,
+  Alert
 } from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import style from './styles';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import Spinner from 'react-native-loading-spinner-overlay';
 
 
 export default class SignUp extends Component {
@@ -20,16 +22,184 @@ export default class SignUp extends Component {
       phone: '',
       address: '',
       password: '',
+      isLoading: false,
     };
   }
 
   componentDidMount () {}
+
+  registerUser () {
+
+    //show spinner
+    this.setState({isLoading: true})
+
+    //strip input of encoded characters
+    let email = this.state.email.replace(/^\s+|\s+$/g, "");
+    let password = this.state.password.replace(/^\s+|\s+$/g, "");
+    let firstname = this.state.firstname.replace(/^\s+|\s+$/g, "");
+    let lastName = this.state.lastName.replace(/^\s+|\s+$/g, "");
+    let phone = this.state.phone.replace(/^\s+|\s+$/g, "");
+    let address = this.state.address.replace(/^\s+|\s+$/g, "");
+    
+    //regex to check that email contains '@' and . and two to fine characters after .
+
+    let checkEmail = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/.test(email);
+
+
+    if (email.length < 1){
+        this.setState({isLoading: false}); //quit loading
+        Alert.alert(
+            'Oops!',
+            'Email cannot be empty',
+            [
+                {
+                  text: 'OK',
+                  style: 'cancel'
+                }
+              ],
+              {
+                cancelable: true
+              }
+         );
+    }
+
+    else if (firstname.length < 1 ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Firstname cannot be empty',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+    else if (lastName.length < 1 ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Lastname cannot be empty',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+    else if (!checkEmail ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Email format is not valid! Ensure to add @ and .',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+    else if (phone.length < 1 ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Phone Number field is empty',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+    else if (phone.length !== 11   ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Phone Numebr Length should be 11 digits',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+    else if (address.length < 1 ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Address field cannot cannot be empty',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+    else if (password.length < 1 ){
+      this.setState({isLoading: false}); //quit loading
+      Alert.alert(
+          'Oops!',
+          'Password field cannot be empty',
+          [
+              {
+                text: 'OK',
+                style: 'cancel'
+              }
+            ],
+            {
+              cancelable: true
+            }
+       )
+    }
+
+
+    else{
+    this.setState({isLoading: false})
+    this.props.navigation.navigate('OTP', firstname)
+    }
+    
+  }
 
   render() {
     console.log(this.state)
     return(
       <View style = {style.container} >
       <StatusBar barStyle = 'dark-content' />
+      
+      <Spinner visible = {this.state.isLoading}
+      textContent = {''}
+      />
+
       <KeyboardAwareScrollView style = {style.wrapper}>
         <View style = {style.input}>
           <TextInput
@@ -45,6 +215,7 @@ export default class SignUp extends Component {
             style = {{fontFamily: 'avertalight'}}
           />
         </View>
+
         <View style = {style.input}>
           <TextInput
             keyboardType = 'default'
@@ -60,6 +231,7 @@ export default class SignUp extends Component {
             style = {{fontFamily: 'avertalight'}}
           />
         </View>
+
         <View style = {style.input}>
           <TextInput
             keyboardType = 'email-address'
@@ -75,6 +247,7 @@ export default class SignUp extends Component {
             style = {{fontFamily: 'avertalight'}}
           />
         </View>
+
         <View style = {style.input}>
           <TextInput
             keyboardType = 'number-pad'
@@ -84,12 +257,14 @@ export default class SignUp extends Component {
             autoCompleteType = 'tel'
             blurOnSubmit = {true}
             textAlign = 'center'
+            maxLength = {11}
             onSubmitEditing = {() => this.address.focus()}
             ref = {(input) => this.telephone = input}
             onChangeText = {(phone) => this.setState({phone})}
             style = {{fontFamily: 'avertalight'}}
           />
         </View>
+
         <View style = {style.input}>
           <TextInput
             keyboardType = 'default'
@@ -105,17 +280,18 @@ export default class SignUp extends Component {
             style = {{fontFamily: 'avertalight'}}
           />
         </View>
+
         <View style = {style.input} >
           <TextInput
             keyboardType = 'default'
-            returnKeyType = 'done'
+            returnKeyType = 'send'
             textContentType = 'password'
             autoCompleteType = 'password'
             placeholder = 'Your Password'
             blurOnSubmit = {true}
             secureTextEntry = {true}
             textAlign = 'center'
-            onSubmitEditing = {() => this.props.navigation.navigate('OTP')}
+            onSubmitEditing = {() => this.registerUser()}
             ref = {(input) => this.password = input}
             onChangeText = {(password) => this.setState({password})}
             style = {{fontFamily: 'avertalight', fontStyle: 'normal',}}
@@ -126,7 +302,7 @@ export default class SignUp extends Component {
 
       <View style = {style.foot}>
 
-        <TouchableOpacity onPress = {() => this.props.navigation.navigate('OTP')} style = {style.reg}>
+        <TouchableOpacity onPress = {() => this.registerUser()} style = {style.reg}>
           <View style ={style.regView} >
             <Text style={style.reg}>Create Account</Text>
           </View>
