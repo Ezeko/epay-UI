d77e8def361196659eb321b6f9b6a5a8462ab52c
Ezeko <ezeko2017@gmail.com>
2020-05-29 03:57:18 +0100

    feat
    create card screen

---
+++ src/screens/Card/style.js
@@ -0,0 +1,7 @@
+import color from "../../Config/color";
+import {Dimensions} from "react-native";
+let width = Dimensions.get("window").width;
+let height = Dimensions.get("window").height;
+
+
+export default {}
